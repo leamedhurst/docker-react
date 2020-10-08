b8b37e1008e8ee7e805b438b1da8f233e8b87214
Lea Medhurst <lea.medhurst@gmail.com>
2020-10-08 13:34:29 +1000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+#build
+FROM node:alpine
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM nginx:alpine
+COPY --from=0 /app/build /usr/share/nginx/html
+
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          By There
+         Hi there changed
         </p>
         <a
           className="App-link"
